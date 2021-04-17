b393b57e1a3736fa4063fc0d21f8b30a3d9d408a
Prestonccc <prestonchen1209@gmail.com>
2021-04-18 02:48:25 +0800

    small change to router

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -3,17 +3,20 @@ const express = require('express')
 // add our router 
 const authorRouter = express.Router()
 
-// add the author controller
+// require the author controller
 const authorController = require('../controllers/authorController.js')
 
 // handle the GET request to get all authors
-authorRouter.get('/', authorController.getAllAuthors)
+authorRouter.get('/', (req, res) => authorController.getAllAuthors(req, res))
 
 // handle the GET request to get one author
-authorRouter.get('/:authorId', authorController.getAuthorByID)
+authorRouter.get('/:authorId', (req, res) => authorController.getOneAuthor(req, res))
 
 // handle POST requests to add one author
-authorRouter.post('/', authorController.addAuthor)
+authorRouter.post('/', (req, res) => authorController.addAuthor(req, res))
+
+// handle POST requests to update an author
+authorRouter.post('/update', (req, res) => authorController.updateAuthor(req, res))
 
 // export the router
 module.exports = authorRouter
